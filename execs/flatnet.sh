cae404132c138c96d22a38293af6adf79ae742c9
siddiquesalman <salmansiddique.khan@gmail.com>
2020-10-18 19:44:55 +0530

    exec edited

--- execs/flatnet.sh
+++ execs/flatnet.sh
@@ -1,4 +1,4 @@
 CHECKPOINT='flatnet'
 MODEL_ROOT='flatnet'
 INIT='Transpose'
-python main.py --wtmse 1 --wtp 0 --wta 0.6 --disPreEpochs 0 --numEpoch 1 --modelRoot $MODEL_ROOT --init $INIT
+python main.py --wtmse 1 --wtp 1.2 --wta 0.2 --disPreEpochs 0 --numEpoch 20 --modelRoot $MODEL_ROOT --init $INIT
